4588df2fbbfe7c99f179ded068cef4f4220852f9
Rocio Burgueño Frias <samuel.dr90@gmail.com>
2018-10-17 21:09:11 +0200

    iteration 3

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -17,6 +17,9 @@ export class LoginComponent implements OnInit {
     private authService: AuthService) { }
 
   ngOnInit() {
+    if (this.authService.isAuthenticated()) {
+      this.router.navigate(['/films'])
+    }
   }
 
   login(){
--- src/app/login/services/auth.service.ts
+++ src/app/login/services/auth.service.ts
@@ -2,17 +2,16 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class AuthService {
-  authToken: string
   constructor() { }
 
   authenticate(user: string, password: string) {
     if(user === "user" && password === "user"){
-      this.authToken = '123456'
-      sessionStorage.setItem('filmsToken', this.authToken);
+      const authToken = '123456'
+      sessionStorage.setItem('filmsToken', authToken);
     }
   }
 
   isAuthenticated(): boolean{
-    return sessionStorage.getItem('filmsToken') === this.authToken
+    return sessionStorage.getItem('filmsToken') === '123456'
   }
 } 
